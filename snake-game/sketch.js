47684bc7851586a84e2dda41eaff50e0161bf43f
thaibalong7 <thai.balong.7@gmail.com>
2019-04-12 15:50:02 +0700

    ! fix snake moves in the opposite direction

--- snake-game/sketch.js
+++ snake-game/sketch.js
@@ -4,7 +4,7 @@ let fr = 10; //starting FPS
 let food;
 let w;
 let h;
-
+let direction = 'right';
 function setup() {
     // put setup code here
     createCanvas(400, 400);
@@ -24,19 +24,38 @@ function getFoodLocation() {
 
 function keyPressed() {
     if (key === 'ArrowLeft') {
-        snake.setDir(-1, 0)
-    }
-    else if (key === 'ArrowRight') {
-        snake.setDir(1, 0)
-    }
-    else if (key === 'ArrowUp') {
-        snake.setDir(0, -1)
+        if (direction !== 'right') {
+            snake.setDir(-1, 0);
+            direction = 'left'
+        }
 
     }
+    else if (key === 'ArrowRight') {
+        if (direction !== 'left') {
+            snake.setDir(1, 0)
+            direction = 'right'
+        }
+    }
+    else if (key === 'ArrowUp') {
+        if (direction !== 'down') {
+            snake.setDir(0, -1)
+            direction = 'up'
+        }
+    }
     else if (key === 'ArrowDown') {
-        snake.setDir(0, 1)
+        if (direction !== 'up') {
+            snake.setDir(0, 1)
+            direction = 'down'
+        }
     }
 }
+function drawWords(x) {
+    // The text() function needs three parameters:
+    // the text to draw, the horizontal position,
+    // and the vertical position
+    fill(0);
+    text('GAME OVER', x, 80);
+  }
 
 function draw() {
     // put drawing code here
@@ -52,6 +71,8 @@ function draw() {
     if (snake.checkEndGame()) {
         console.log('END GAME');
         background(0, 0, 255);
+        textAlign(CENTER);
+        drawWords(width * 0.5);
         noLoop();
     }
 
